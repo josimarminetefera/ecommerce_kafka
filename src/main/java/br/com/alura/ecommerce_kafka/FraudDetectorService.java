ff8091dac90ed5f0c722563231cf0feea40f86c1
Josimar Minete <josimaminete@gmail.com>
2021-11-29 18:51:25 -0300

    Adiciona classes para produtor e consumidor

---
+++ src/main/java/br/com/alura/ecommerce_kafka/FraudDetectorService.java
@@ -0,0 +1,62 @@
+package br.com.alura.ecommerce_kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class FraudDetectorService {
+    public static void main(String[] args) {
+        //CRIAR O CNSUMIDOR DAS MENSAGENS
+        KafkaConsumer consumer = new KafkaConsumer<String, String>(properties());
+        //PARA CONSUMIR A MENSAGEM subscribe
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+        System.out.println("Iniciando...........");
+        while (true) {
+            //VERIFICAR SE TEM MENSAGEM DENTRO DO CONSUMIDOR ISSO RETORNA VÁRIOS REGISTROS
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            if (!records.isEmpty()) {
+                System.out.println("Verificando.................");
+                System.out.println("Encontrei " + records.count() + " registros!");
+
+                for (ConsumerRecord<String, String> record : records) {
+                    System.out.println("-----------------------------------------------------");
+                    System.out.println("Procesando new order: ");
+                    System.out.println(record.key());
+                    System.out.println(record.value());
+                    System.out.println(record.partition());
+                    System.out.println(record.offset());
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println("New order processada");
+                }
+
+            }
+
+        }
+    }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+
+        //ONDE VAI BUSCAR AS MENSAGENS
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+
+        //AS KEY E VALUE TEM QUE SER DESCRIPTOGRAFADO DE BYTE PARA STRING
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        //TEM QUE FALAR O ID DO GRUPO QUE NESTE CASO É O NOME DA CLASS
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+        return properties;
+    }
+}
---
+++ src/main/java/br/com/alura/ecommerce_kafka/NewOrderMain.java
@@ -0,0 +1,52 @@
+package br.com.alura.ecommerce_kafka;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class NewOrderMain {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //ENVIAR UMA MENSAGEM NO KAFKA
+        KafkaProducer producer = new KafkaProducer<String, String>(properties());
+
+        //MENSAGEM QUE EU QUERO MANDAR
+        String value = "132123,67523,10000";
+
+        //CRIAR REGISTRO DO PRODUCER
+        ProducerRecord record = new ProducerRecord("ECOMMERCE_NEW_ORDER", value, value);
+
+        //CALBACK PARA AGUARDAR E PEGAR A RESPOSTA ASSIM QUE CHEGAR
+        Callback callback = (data, ex) -> {
+            //ESCUTAR O RETORNO DA MENSAGEM
+            if (ex != null) {
+                ex.printStackTrace();
+                return;
+            }
+            System.out.println("sucesso " + data.topic() + ":::partition " + data.partition() + " /offset " + data.offset() + " /timestamp " + data.timestamp());
+        };
+
+        //PARA ENVIAR UMA MESAGEM send
+        //ENVIAR MENSAGEM E COM get()
+        producer.send(record, callback).get();
+
+        String email = "Obrigado por sua New Order!";
+        ProducerRecord<String,String> emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        //ENVIAR UM E-MAIL PARA VERIFICAR
+        producer.send(emailRecord, callback).get();
+    }
+
+    private static Properties properties() {
+        Properties properties = new Properties();
+        //ONDE VAI CONECTAR O KAFKA
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        //TEM QUE TRANSFORMAR A KEY E O VALOR DO KafkaProducer DE STRING PARA BYTE
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        return properties;
+    }
+}
